c8cb8b0009995688d6dd1ad998a6a0287395a238
Myrmex <dfusi@hotmail.com>
2020-01-03 09:58:32 +0100

    Added seed command doc

--- CadmusTool/Commands/SeedDatabaseCommand.cs
+++ CadmusTool/Commands/SeedDatabaseCommand.cs
@@ -21,9 +21,11 @@ namespace CadmusTool.Commands
         private readonly string _database;
         private readonly string _profilePath;
         private readonly int _count;
+        private readonly bool _dry;
+        private readonly bool _history;
 
         public SeedDatabaseCommand(AppOptions options, string database,
-            string profilePath, int count)
+            string profilePath, int count, bool dry, bool history)
         {
             _config = options.Configuration;
             _repositoryService = new RepositoryService(_config);
@@ -33,6 +35,8 @@ namespace CadmusTool.Commands
             _profilePath = profilePath
                 ?? throw new ArgumentNullException(nameof(profilePath));
             _count = count;
+            _dry = dry;
+            _history = history;
         }
 
         public static void Configure(CommandLineApplication command,
@@ -48,8 +52,16 @@ namespace CadmusTool.Commands
             CommandArgument profileArgument = command.Argument("[profile]",
                 "The seed profile JSON file path");
 
-            CommandOption countOption = command.Option("-c|--count", "Items count",
+            CommandOption countOption = command.Option("-c|--count",
+                "Items count (default=100)",
                 CommandOptionType.SingleValue);
+
+            CommandOption dryOption = command.Option("-d|--dry", "Dry run",
+                CommandOptionType.NoValue);
+
+            CommandOption historyOption = command.Option("-h|--history",
+                "Add history data",
+                CommandOptionType.NoValue);
 
             command.OnExecute(() =>
             {
@@ -62,7 +74,8 @@ namespace CadmusTool.Commands
                 options.Command = new SeedDatabaseCommand(options,
                     databaseArgument.Value,
                     profileArgument.Value,
-                    count);
+                    count,
+                    dryOption.HasValue());
                 return 0;
             });
         }
@@ -75,53 +88,66 @@ namespace CadmusTool.Commands
 
         public Task Run()
         {
-            Console.WriteLine("SEED DATABASE\n" +
-                              $"Database: {_database}\n" +
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("SEED DATABASE\n");
+            Console.ResetColor();
+
+            Console.WriteLine($"Database: {_database}\n" +
                               $"Profile file: {_profilePath}\n" +
-                              $"Count: {_count}\n");
+                              $"Count: {_count}\n" +
+                              $"Dry run: {_dry}\n" +
+                              $"History: {_history}\n");
             Serilog.Log.Information("SEED DATABASE: " +
                          $"Database: {_database}, " +
                          $"Profile file: {_profilePath}, " +
-                         $"Count: {_count}");
+                         $"Count: {_count}, " +
+                         $"Dry: {_dry}, " +
+                         $"History: {_history}");
 
-            // create database if not exists
-            string connection = string.Format(CultureInfo.InvariantCulture,
-                _config.GetConnectionString("Mongo"),
-                _database);
-
-            IDatabaseManager manager = new MongoDatabaseManager();
-
-            string profileContent = LoadProfile(_profilePath);
-            IDataProfileSerializer serializer = new JsonDataProfileSerializer();
-            DataProfile profile = serializer.Read(profileContent);
-
-            if (!manager.DatabaseExists(connection))
+            if (!_dry)
             {
-                Console.WriteLine("Creating database...");
-                Serilog.Log.Information($"Creating database {_database}...");
+                // create database if not exists
+                string connection = string.Format(CultureInfo.InvariantCulture,
+                    _config.GetConnectionString("Mongo"),
+                    _database);
 
-                manager.CreateDatabase(connection, profile);
+                IDatabaseManager manager = new MongoDatabaseManager();
 
-                Console.WriteLine("Database created.");
-                Serilog.Log.Information("Database created.");
+                string profileContent = LoadProfile(_profilePath);
+                IDataProfileSerializer serializer = new JsonDataProfileSerializer();
+                DataProfile profile = serializer.Read(profileContent);
+
+                if (!manager.DatabaseExists(connection))
+                {
+                    Console.WriteLine("Creating database...");
+                    Serilog.Log.Information($"Creating database {_database}...");
+
+                    manager.CreateDatabase(connection, profile);
+
+                    Console.WriteLine("Database created.");
+                    Serilog.Log.Information("Database created.");
+                }
             }
 
             Console.WriteLine("Creating repository...");
             Serilog.Log.Information("Creating repository...");
 
-            ICadmusRepository repository =
-                _repositoryService.CreateRepository(_database);
+            ICadmusRepository repository = _dry
+                ? null : _repositoryService.CreateRepository(_database);
 
             Console.WriteLine("Seeding items");
             PartSeederFactory factory = _seederService.GetFactory(_profilePath);
             CadmusSeeder seeder = new CadmusSeeder(factory);
             foreach (IItem item in seeder.GetItems(_count))
             {
-                Console.WriteLine(item);
-                repository.AddItem(item, false);
-                foreach (IPart part in item.Parts)
+                Console.WriteLine($"{item}: {item.Parts.Count} parts");
+                if (!_dry)
                 {
-                    repository.AddPart(part, false);
+                    repository.AddItem(item, _history);
+                    foreach (IPart part in item.Parts)
+                    {
+                        repository.AddPart(part, _history);
+                    }
                 }
             }
             Console.WriteLine("Completed.");
---
+++ CadmusTool/Readme.md
@@ -0,0 +1,28 @@
+﻿# CadmusTool
+
+Cadmus configuration and utility tools.
+
+## Seed Database Command
+
+This command is used to create a new Cadmus MongoDB database (if the specified database does not already exists), and seed it with a specified number of random items.
+
+This command relies on plugins in the `Plugins` folder, so be sure to run the `UpdatePlugins` batch, or manually update the plugins before executing it.
+
+For a sample seed profile see `Assets/SeedProfile.json`.
+
+Syntax:
+
+```ps1
+./CadmusTool.exe seed <MongoDBName> <SeedProfileFilePath> [-c ItemCount] [-d] [-h]
+```
+
+where:
+
+- `MongoDBName` is the name of the MongoDB database to be seeded (and created, if it does not exist).
+- `SeedProfileFilePath` is the path to the seed profile JSON file.
+
+Options:
+
+- `-c N` or `--count N`: the number of items to be seeded. Default is 100.
+- `-d` or `--dry`: dry run, i.e. create the items and parts, but do not create the database nor store anything into it. This is used to test for seeder issues before actually running it.
+- `-h` or `--history`: add history items and parts together with the seeded items and parts. Default is `false`. In a real-world database you should set this to `true`.
