babee8c4af78b1adfc24775a9fc3412f71062c18
Myrmex <dfusi@hotmail.com>
2020-01-29 15:47:42 +0100

    Updated packages

--- UpdateLocalPackages.bat
+++ UpdateLocalPackages.bat
@@ -12,10 +12,10 @@ xcopy %src%messagingapi\1.0.1\*.nupkg .\local-packages\ /y
 xcopy %src%messagingapi.sendgrid\1.0.1\*.nupkg .\local-packages\ /y
 xcopy %src%cadmus.core\2.2.12\*.nupkg .\local-packages\ /y
 xcopy %src%cadmus.mongo\2.2.14\*.nupkg .\local-packages\ /y
-xcopy %src%cadmus.parts\2.2.14\*.nupkg .\local-packages\ /y
+xcopy %src%cadmus.parts\2.2.15\*.nupkg .\local-packages\ /y
 xcopy %src%cadmus.philology.parts\2.2.16\*.nupkg .\local-packages\ /y
 xcopy %src%cadmus.seed\1.0.13\*.nupkg .\local-packages\ /y
-xcopy %src%cadmus.seed.parts\1.0.14\*.nupkg .\local-packages\ /y
+xcopy %src%cadmus.seed.parts\1.0.15\*.nupkg .\local-packages\ /y
 xcopy %src%cadmus.seed.philology.parts\1.0.14\*.nupkg .\local-packages\ /y
 xcopy %src%cadmus.archive.parts\2.2.12\*.nupkg .\local-packages\ /y
 xcopy %src%cadmus.lexicon.parts\2.2.13\*.nupkg .\local-packages\ /y
