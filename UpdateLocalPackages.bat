5572f6823e5ea1b94d4dab563c00037239de6575
Myrmex <dfusi@hotmail.com>
2020-05-04 13:21:45 +0200

    Updated packages

--- UpdateLocalPackages.bat
+++ UpdateLocalPackages.bat
@@ -15,18 +15,18 @@ xcopy %src%fusi.microsoft.extensions.configuration.inmemoryjson\1.0.1\*.nupkg .\
 xcopy %src%messagingapi\1.0.1\*.nupkg .\local-packages\ /y
 xcopy %src%messagingapi.sendgrid\1.0.2\*.nupkg .\local-packages\ /y
 
-xcopy %src%cadmus.core\2.2.35\*.nupkg .\local-packages\ /y
-xcopy %src%cadmus.index\1.0.11\*.nupkg .\local-packages\ /y
-xcopy %src%cadmus.index.sql\1.0.32\*.nupkg .\local-packages\ /y
-xcopy %src%cadmus.mongo\2.2.45\*.nupkg .\local-packages\ /y
+xcopy %src%cadmus.core\2.2.36\*.nupkg .\local-packages\ /y
+xcopy %src%cadmus.index\1.0.12\*.nupkg .\local-packages\ /y
+xcopy %src%cadmus.index.sql\1.0.33\*.nupkg .\local-packages\ /y
+xcopy %src%cadmus.mongo\2.2.46\*.nupkg .\local-packages\ /y
 
-xcopy %src%cadmus.parts\2.2.43\*.nupkg .\local-packages\ /y
-xcopy %src%cadmus.philology.parts\2.2.43\*.nupkg .\local-packages\ /y
+xcopy %src%cadmus.parts\2.2.44\*.nupkg .\local-packages\ /y
+xcopy %src%cadmus.philology.parts\2.2.44\*.nupkg .\local-packages\ /y
 
-xcopy %src%cadmus.seed\1.0.38\*.nupkg .\local-packages\ /y
-xcopy %src%cadmus.seed.parts\1.0.43\*.nupkg .\local-packages\ /y
-xcopy %src%cadmus.seed.philology.parts\1.0.40\*.nupkg .\local-packages\ /y
+xcopy %src%cadmus.seed\1.0.39\*.nupkg .\local-packages\ /y
+xcopy %src%cadmus.seed.parts\1.0.44\*.nupkg .\local-packages\ /y
+xcopy %src%cadmus.seed.philology.parts\1.0.41\*.nupkg .\local-packages\ /y
 
-xcopy %src%cadmus.archive.parts\2.2.36\*.nupkg .\local-packages\ /y
-xcopy %src%cadmus.lexicon.parts\2.2.37\*.nupkg .\local-packages\ /y
+xcopy %src%cadmus.archive.parts\2.2.37\*.nupkg .\local-packages\ /y
+xcopy %src%cadmus.lexicon.parts\2.2.38\*.nupkg .\local-packages\ /y
 pause
