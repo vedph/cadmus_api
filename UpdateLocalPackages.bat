9861f72def59016dc4df2ac78b547125e4999560
Myrmex <dfusi@hotmail.com>
2020-08-13 22:51:23 +0200

    Updated packages

--- UpdateLocalPackages.bat
+++ UpdateLocalPackages.bat
@@ -5,27 +5,27 @@ set src=C:\Projects\_NuGet\
 del .\local-packages\*.nupkg
 xcopy %src%diffmatchpatch\1.0.1\*.nupkg .\local-packages\ /y
 
-xcopy %src%fusi.tools\1.1.14\*.nupkg .\local-packages\ /y
-xcopy %src%fusi.tools.config\1.0.15\*.nupkg .\local-packages\ /y
+xcopy %src%fusi.tools\1.1.15\*.nupkg .\local-packages\ /y
+xcopy %src%fusi.tools.config\1.0.16\*.nupkg .\local-packages\ /y
 xcopy %src%fusi.text\1.1.11\*.nupkg .\local-packages\ /y
-xcopy %src%fusi.antiquity\1.1.24\*.nupkg .\local-packages\ /y
+xcopy %src%fusi.antiquity\1.1.25\*.nupkg .\local-packages\ /y
 
 xcopy %src%fusi.microsoft.extensions.configuration.inmemoryjson\1.0.1\*.nupkg .\local-packages\ /y
 
 xcopy %src%messagingapi\1.0.1\*.nupkg .\local-packages\ /y
 
-xcopy %src%cadmus.core\2.2.41\*.nupkg .\local-packages\ /y
-xcopy %src%cadmus.index\1.0.17\*.nupkg .\local-packages\ /y
-xcopy %src%cadmus.index.sql\1.0.39\*.nupkg .\local-packages\ /y
-xcopy %src%cadmus.mongo\2.2.51\*.nupkg .\local-packages\ /y
+xcopy %src%cadmus.core\2.2.42\*.nupkg .\local-packages\ /y
+xcopy %src%cadmus.index\1.0.18\*.nupkg .\local-packages\ /y
+xcopy %src%cadmus.index.sql\1.0.40\*.nupkg .\local-packages\ /y
+xcopy %src%cadmus.mongo\2.2.52\*.nupkg .\local-packages\ /y
 
-xcopy %src%cadmus.parts\2.2.49\*.nupkg .\local-packages\ /y
-xcopy %src%cadmus.philology.parts\2.2.55\*.nupkg .\local-packages\ /y
+xcopy %src%cadmus.parts\2.2.50\*.nupkg .\local-packages\ /y
+xcopy %src%cadmus.philology.parts\2.2.56\*.nupkg .\local-packages\ /y
 
-xcopy %src%cadmus.seed\1.0.44\*.nupkg .\local-packages\ /y
-xcopy %src%cadmus.seed.parts\1.0.52\*.nupkg .\local-packages\ /y
-xcopy %src%cadmus.seed.philology.parts\1.0.52\*.nupkg .\local-packages\ /y
+xcopy %src%cadmus.seed\1.0.45\*.nupkg .\local-packages\ /y
+xcopy %src%cadmus.seed.parts\1.0.53\*.nupkg .\local-packages\ /y
+xcopy %src%cadmus.seed.philology.parts\1.0.53\*.nupkg .\local-packages\ /y
 
-xcopy %src%cadmus.archive.parts\2.2.42\*.nupkg .\local-packages\ /y
-xcopy %src%cadmus.lexicon.parts\2.2.43\*.nupkg .\local-packages\ /y
+xcopy %src%cadmus.archive.parts\2.2.43\*.nupkg .\local-packages\ /y
+xcopy %src%cadmus.lexicon.parts\2.2.44\*.nupkg .\local-packages\ /y
 pause
